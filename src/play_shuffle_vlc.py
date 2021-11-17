59864a4b931f1c421cd1f5cf42e474c95ee3bf1a
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2021-11-18 00:46:48 +0100

    First

---
+++ src/play_shuffle_vlc.py
@@ -0,0 +1,30 @@
+from play_shuffle import play_shuffle
+
+def play_shuffle_VLC():
+    
+    play_shuffle()
+    # imprimir_canciones()
+    # lanzarVLC()
+
+
+playList = {}
+# playList ={ 1: "titulo cancion", 2: "titulo cancion" ... }
+
+libreria = {"California_Uber_Alles.mp3":
+                {"track-number": 3,
+                 "artist": "Dead Kennedys",
+                 "album": "Dead Kennedys",
+                 "location": "./biblioteca/California_Uber_Alles.mp3"},
+            "Seattle_Party": 
+                {"track-number": 1,
+                "artist": "Chastity Belt",
+                "album": "No regrets",
+                "location": "./biblioteca/Seattle_Party.flac"},
+            "King_Kunta":
+                {"track-number": 3,
+                "artist": "Kendrick Lamar",
+                "album": "To Pimp A Butterfly",
+                "location": "./biblioteca/King_Kunta.mp3"}   
+            }
+
+play_shuffle_VLC()
