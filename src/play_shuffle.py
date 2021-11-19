3426acc5c10214ac7a3adc14a1b4797cdcb4e7ee
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2021-11-19 15:22:33 +0100

    requierements.txt

--- src/play_shuffle.py
+++ src/play_shuffle.py
@@ -3,9 +3,10 @@ from seleccionar_cancion_random import seleccionar_cancion_random
 
 def play_shuffle(libreria, playlist):
     
+    # Comprobacion de variables
     assert isinstance(libreria, dict)
     assert isinstance(playlist, dict)
-    
+
 
     iniciar_playlist()
     print(__name__)
